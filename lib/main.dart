20ab666fb4c2e8c77a49d49f8342081eae1ba066
Muhammad Ghifar <muhammad.ghifar@gmail.com>
2026-07-10 00:34:56 +0700

    Refactor todo list using Todo model

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,30 @@ void main() {
   runApp(MyApp());
 }
 
+class Todo {
+  String title;
+  bool isDone;
+
+  Todo({
+    required this.title, 
+    required this.isDone,
+  });
+}
+
 class MyApp extends StatelessWidget {
-  final List<String> todoList = [
-    'Belajar Flutter',
-    'Belajar Git',
-    'Push ke GitHub',
+  final List<Todo> todoList = [
+    Todo(
+      title: 'Belajar Flutter',
+      isDone: false,
+    ),
+    Todo(
+      title: 'Belajar Git',
+      isDone: false,
+    ),
+    Todo(
+      title: 'Push ke GitHUb',
+      isDone: false,
+    ),
   ];
 
   MyApp({super.key});
@@ -34,12 +53,15 @@ class MyApp extends StatelessWidget {
                   child: ListView.builder(
                     itemCount: todoList.length,
                     itemBuilder: (context, index) {
-                      return Row(
-                        children: [
-                          const Icon(Icons.check_box_outline_blank),
-                          const SizedBox(width: 8),
-                          Text(todoList[index])
-                        ]
+                      final todo = todoList[index];
+                      return ListTile(
+                        leading: 
+                          Icon(
+                            todo.isDone 
+                            ? Icons.check_box 
+                            : Icons.check_box_outline_blank),
+                        title: Text(todo.title),
+                        trailing: const Icon(Icons.delete_outline),
                       );
                     }
                   ),
